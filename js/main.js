7120748b090230524b5a8660a186056134fecd2b
Tom VanAntwerp <vanantwerp@gmail.com>
2015-06-21 14:02:33 -0400

    Prep JS structure for app

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict'
+
+  var nextSlide, previousSlide;
+
+  nextSlide = function() {
+    //TODO write next slide function
+  };
+
+  previousSlide = function() {
+    //TODO write previous slide function
+  };
+});
