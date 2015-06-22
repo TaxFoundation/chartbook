0684bde60ef6d8f9fd8315bf7c96850642df2764
Tom VanAntwerp <vanantwerp@gmail.com>
2015-06-22 14:44:52 -0400

    Use collection for slides, fixes #5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 (function() {
   'use strict'
 
-  var nextSlide, previousSlide;
+  var setSlide, nextSlide, previousSlide;
+
+  setSlide = function() {
+    //TODO write function to find initial slide based on URL and set it
+  };
 
   nextSlide = function() {
     //TODO write next slide function
